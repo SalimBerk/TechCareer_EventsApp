d02006598af82aced0f5921d9b8755b6ed7ad9d8
SalimBerk <77536512+SalimBerk@users.noreply.github.com>
2023-08-30 20:55:40 +0300

    I included Sass in my project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./style/main.css";
 import { Route, Routes } from "react-router-dom";
 import { Contact } from "./Pages/Contact";
 import { About } from "./Pages/About";
